d4e8e85200fa7484a8c5dffe6cb53eb37ed1465f
Ankur Charan <ankurcharan98@gmail.com>
2018-11-11 23:44:38 +0530

    setting up post request
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
+	res.render('index');
+})
+
+app.post('/', function (req, res) {
+	console.log(req.body.city);
 	res.render('index');
 })
 
